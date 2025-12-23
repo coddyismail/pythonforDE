bc050bbd609166d6bf538a07cb213455c638d5a9
aakher <kymalum20@gmail.com>
2025-12-23 23:34:25 +0530

    first comit

---
+++ first.py
@@ -0,0 +1,5 @@
+#count function
+greeting = "hello coder, where are you coder. when youll finsh this, coder?"
+print(greeting.count("coder"))
+print(greeting.isnumeric())
+print(greeting.isstring())
