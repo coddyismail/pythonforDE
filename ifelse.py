6a525ab59db21074af18cfaa35638be5aae6e72b
aakher <kymalum20@gmail.com>
2025-12-23 23:49:07 +0530

    2nd commit learnt ifelse

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+x = 110
+if(x==10):
+    print("x is 10")
+elif(x>100):
+    if(x > 100 & x < 200):
+        print("something between 100 - 200")
+
+else:
+    print("x ain't 10")   
+
+#completed    
